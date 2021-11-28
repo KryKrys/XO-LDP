f29b9fb2826e82446f856810840a58bd965d3997
KryKrys <krymel24@hotmail.com>
2021-11-28 14:38:16 -0600

    lolazo

--- graphics.c
+++ graphics.c
@@ -7,18 +7,29 @@
 
 void main(){
     int driver=DETECT,modo,x,y;
-    button boton1;
+    /*button boton1;*/
     initgraph(&driver,&modo,"");
-    newButton(&boton1,100,100,"Ajustes");
-    setbkcolor(DARKGRAY);
-    rectangle(170,90,470,390);
-    /*verticales*/
+    
+    setbkcolor(WHITE);
+    setcolor(WHITE);
+    rectangle(0,0,639,479);
+    setfillstyle(CLOSE_DOT_FILL,MAGENTA);
+    floodfill(10,10,WHITE);
+    setcolor(DARKGRAY);
+    setfillstyle(SOLID_FILL,BROWN);
+    bar(100,70,530,410);
+    getch();
+
+    /*rectangle(170,90,470,390);
+    /*verticales*
     line(270,90,270,390);
     line(375,90,375,390);
-    /*horizontales*/
+    /*horizontales*
     line(170,190,470,190);
     line(170,290,470,290);
     
+    newButton(&boton1,100,100,"Ajustes");
+    mver();
 
     do{
 		while(mclick()!=1){
@@ -30,12 +41,11 @@ void main(){
 				mocultar();
 				effect3d(boton1.x1,boton1.y1,boton1.x2,boton1.y2,1,DEPRESSED);
 				setcolor(WHITE);
-				outtextxy(300,300,"Hola B(");
+				outtextxy(100,120,"Hola B(");
 				mver();
 			}
 		}
 		effect3d(boton1.x1,boton1.y1,boton1.x2,boton1.y2,1,ELEVATE);
-	}while(getch()==' ');
-    getch();
+	}while(mclick()!=2);*/
     closegraph();
 }
