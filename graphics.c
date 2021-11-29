8b970bd2cb0ce06fc7f485a1e7210ed2077f5502
KryKrys <krymel24@hotmail.com>
2021-11-28 22:12:55 -0600

    ajustes 10:11pm

--- graphics.c
+++ graphics.c
@@ -5,22 +5,35 @@
 #include<stdio.h>
 #include<button.h>
 
+void ajustes(void);
+void pantalla_princip(void);
+
 void main(){
-    int driver=DETECT,modo=VGAHI,x,y;
+    int driver=DETECT,modo=VGAHI;
     /*button boton1;*/
     initgraph(&driver,&modo,"");
     /*fondo*/
+    pantalla_princip();
+
+    closegraph();
+}
+
+void pantalla_princip(){
+    int x,y;
+    clrscr();
+    setfillstyle(SOLID_FILL,LIGHTGRAY);
+    bar(0,0,640,480);
     setbkcolor(LIGHTGRAY);
     setcolor(WHITE);
 
     /*letras*/
     setcolor(BLUE);
-    settextstyle(DEFAULT_FONT,0,5);
-    outtextxy(100,20,"Tic");
+    settextstyle(DEFAULT_FONT,0,4);
+    outtextxy(100,20,"Tres");
     setcolor(LIGHTBLUE);
-    outtextxy(250,20,"Tac");
+    outtextxy(260,20,"en");
     setcolor(CYAN);
-    outtextxy(400,20,"Toe");
+    outtextxy(375,20,"Linea");
     /*circulo y cruz*/
     setcolor(GREEN);
     setlinestyle(0,1,3);
@@ -43,10 +56,69 @@ void main(){
     outtextxy(260,180,"Jugar");
     outtextxy(260,280,"Salir");
 
+    mver();
+    do{
+        while(mclick()!=1){
+            x = mxpos(1);
+            y = mypos(1);
+        }
 
-    getch();
+        if(limit(x,y,215,155,425,225)){
+            mocultar();
+            ajustes();
+        }
 
+        if(limit(x,y,215,255,425,325)){
+            mocultar();
+            closegraph();
+            exit(0);
+        }
 
+    }while(1);
+}
+
+void ajustes(){
+    int x,y;
+    clrscr();
+    setbkcolor(LIGHTGRAY);
+    setfillstyle(SOLID_FILL,LIGHTGRAY);
+    bar(0,0,640,480);
+
+    /*menu*/
+    setfillstyle(LINE_FILL,WHITE);
+    bar(250,70,570,420);
+    setfillstyle(SOLID_FILL,CYAN);
+    bar(270,90,550,400);
+    setcolor(DARKGRAY);
+    settextstyle(0,0,4);
+    outtextxy(250,20,"Modalidad");
+    /*regresar*/
+    setfillstyle(SOLID_FILL,CYAN);
+    bar(10,10,90,40);
+    setcolor(DARKGRAY);
+    line(24,25,80,25);
+    line(24,25,43,35);
+    line(24,25,43,15);
+
+    settextstyle(0,0,3);
+    outtextxy(290,110,"P1 vs PC");
+    outtextxy(290,190,"P1 vs P2");
+
+    mver();
+    do{
+        while(mclick()!=1){
+            x = mxpos(1);
+            y = mypos(1);
+        }
+
+        if(limit(x,y,5,5,95,45)){
+            mocultar();
+            pantalla_princip();
+        }
+
+    }while(1);
+
+}
 
     /*rectangle(170,90,470,390);
     /*verticales*
@@ -75,5 +147,3 @@ void main(){
 		}
 		effect3d(boton1.x1,boton1.y1,boton1.x2,boton1.y2,1,ELEVATE);
 	}while(mclick()!=2);*/
-    closegraph();
-}
